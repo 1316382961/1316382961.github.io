dd60db5f6a10cb855f12fdc2cf71f2b3032cc753
DS <36700965+1316382961@users.noreply.github.com>
2020-11-10 15:28:48 +0800

    update

--- JS/index.js
+++ JS/index.js
@@ -7,18 +7,18 @@ swch.addEventListener ("click",(e)=>{
 
     //可以实用切换  toggle 是jqery中响应被选元素的 click 事件之间的切换
     // swch.classList.toggle('switched');
-
     //为后期留预备空间使用循环
     //contains为jqery判断指定元素内是否包含另一个元素
     if(swch.classList.contains('switched')){
-
         // 删除样式
         swch.classList.remove('switched');
+      
 
     }else{
         // 增加样式
         swch.classList.add("switched");
-        alert("开启快乐每一天");
+        alert("开启快乐每一天"); 
+        
+        
     }
 })
-
